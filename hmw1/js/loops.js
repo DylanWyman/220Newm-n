a27bc7066e480c302c5a0c31e5baa7ddb6eda09a
DylanWyman <74611535+DylanWyman@users.noreply.github.com>
2021-05-19 13:41:15 -0400

    recreating the file because visual studio code glitched out.

---
+++ hmw1/js/loops.js
@@ -0,0 +1,37 @@
+new p5();
+var item = 26;
+var xloc = 100;
+var num = 1;
+
+
+
+var boop = random(150);
+
+function setup() {
+    createCanvas(500, 500);
+}
+
+function draw() {
+    background(boop, 155, boop);
+
+    while (item>=1) {
+        item--;
+        if ((1=item/3),(2=item/3),(3=item/3),(4=item/3),(6=item/3),(7=item/3),(8=item/3)) {
+            fill ( );
+            ellipse(xloc,500,20,20);
+        };
+        if ((1=item/5),(2=item/5),(4=item/5),(5=item/5))
+        if (5=item/3) {
+
+            ellipse(xloc,500,20,20);
+        };
+        if ((item=1),(item=2),(item=4),(item=7),(item=8),(item=11),(item=13),(item=14),(item=16),(item=17),(item=19),(item=22),(item=23)) {
+            fill (255,255,255);
+            ellipse(xloc,500,20,20);
+        };
+        xloc = (xloc+50)
+    }
+  
+  
+    
+}
