a57274b56c631b7927a271948e6f583c61f838bb
DylanWyman <74611535+DylanWyman@users.noreply.github.com>
2021-05-26 21:36:50 -0400

    hmw3

---
+++ hmw1/js/arrays/arr2.js
@@ -0,0 +1,16 @@
+new p5();
+
+var randomizeBackground = random(150);
+
+function setup() {
+    createCanvas(1000, 1000);
+}
+
+function draw() {
+    background(155, randomizeBackground, randomizeBackground);
+
+    
+    
+}
+
+//drop bricks
