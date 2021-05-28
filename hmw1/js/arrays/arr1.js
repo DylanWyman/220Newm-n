06a25e5827723ab8ca7870b96173ea605f788457
DylanWyman <74611535+DylanWyman@users.noreply.github.com>
2021-05-27 22:04:24 -0400

    hmw4 finished

--- hmw1/js/arrays/arr1.js
+++ hmw1/js/arrays/arr1.js
@@ -1,62 +1,41 @@
 new p5();
 
+
+//randomly color the background
 var randomizeBackground = random(150);
 
-
-/*
-    for(var i = arr.length - 1; i>0; i--){
-        const j = Math.floor(Math.random()*(i+1))
-        [array[i],array[j]] = [array[j], array[i]]
-    }   
-
-    function shuffle(array){
-    for(var i = arr.length - 1; i>0; i--){
-        const j = Math.floor(Math.random()*(i+1))
-        [array[i],array[j]] = [array[j], array[i]]
-    }     
-}
-*/
+//set an x position
+var x=20;
 
 
-
-
-
+//rules of the canvas
 function setup() {
     createCanvas(1000, 1000)
 
 
 
-
+    frameRate(1);
 }
 
+//start drawing
 function draw() {
-    //console.log(colors);
 
-    //var colors = ["#052abf", "#0d8b04", "#92d11a", "#a1e2d3", "#771c4a", "#0b1a19", "#f47de6", "#ffb760"];
-   // print(colors);
-    //shuffle(colors, true); // force modifications to passed array
-   // print(colors);
-  
-    // By default shuffle() returns a shuffled cloned array:
-    //let newColors = shuffle(colors);
-   // print(colors);
-   // print(newColors);
+    //colors for icecream
+    var colors = ["#92d11a", "#a1e2d3", "#771c4a"];
 
 
-
-
-
-
-
-
-
+//color the background
     background(155, randomizeBackground, randomizeBackground);
 
-    for(var i=0;i<3;i=i++){
-        //create circles
-        //fill(newColors);
-        //fill(122,122,0);
-        rect(i+10,10,45,145);
+
+    //color loop
+    for(var i=0;i<colors.length;i++){
+        //fill color based on array
+        fill(colors[i]);
+        //create a rectangle
+        rect(x+10,10,45,145);
+        //move the next rectangle
+        x=x+55;
 
     }
 
